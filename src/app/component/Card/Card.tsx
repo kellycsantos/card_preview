01cf19193a6fe2729ce405a3c55d918cc432f970
Kelly C Santos <kellycristinasntos@gmail.com>
2025-11-07 17:16:36 -0300

    feat: configurated props and card styles

--- src/app/component/Card/Card.tsx
+++ src/app/component/Card/Card.tsx
@@ -1,9 +1,22 @@
-const Card = () => { 
-    return (
-        <div className="card">
+import styles from "./Card.module.scss";
 
-        </div>
-    )
+type CardProps = {
+    image: string;
+    alt: string;
+    title: string;
+    description: string;
+
 }
 
-export default Card;
+export const Card = ({ image,alt, title, description }: CardProps) => {
+    return (
+        <section className={styles.card}>
+            <img className={styles.card__image} src={image} alt={alt} />
+
+            <article className={styles.card__content}>
+                <h1>{title}</h1>
+                <p>{description}</p>
+            </article>
+        </section>
+    )
+}
